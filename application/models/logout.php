6db1a2b93f764b18ca61806d581e8d8506f4e665
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2018-10-23 13:17:26 +0300

    Add files via upload

---
+++ application/models/logout.php
@@ -0,0 +1,19 @@
+<html><head>
+<link href="css/admin_styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body bgcolor="tan">
+<center><b><font color = "brown" size="6">Online Voting System</font></b></center><br><br>
+<div id="page">
+<div id="header">
+<h1>Logged Out Successfully </h1>
+<p align="center">&nbsp;</p>
+</div>
+<?
+session_start();
+session_destroy();
+?>
+You have been successfully logged out .<br><br><br>
+Return to <a href="login.html">Login</a>
+
+ --></div>
+</body></html>
